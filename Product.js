58d9d779cbdacd6a6c4e9a9f4288549e561de04f
Nouman Waheed <74645642+Nomi444@users.noreply.github.com>
2021-03-26 18:22:26 +0500

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,26 +1,42 @@
-import {products} from '../../Data/product';
+import{Link
+  } from "react-router-dom";
 
-const getVisibleProducts= (link) => {
+export default function Productmap({incrementCart, addToCart  , pro })
+{
     
-    return (
-      
-    products.filter(p => p.link === link)
-      
-    );
-} 
-const getVisibleProductsbyId= (id) => {
-  
-  return (
-    
+   return(
+    <div className="container mt-4">
+    <div className="row justify-content-center">
+      <div className="card mb-4 col-lg-6 col-sm-8">
+        <img className="card-img-top img-fluid" style={{width :"30%"}}src={pro.img}  
+        />
+        <div className="card-body">
+          <h3 className="card-title">{pro.name}</h3>
+          <h4>{pro.price}</h4>
+          <p className="card-text">{pro.description}</p>
+          <span className="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
+          4.0 stars
+          <br/>
+          <br/>
+          <button onClick={() => incrementCart(pro)} type="button" className="btn  btn-primary mr-2">Add this item</button>
+          <Link to={"/collection/category/" + pro.id}
+                        type="button"
+                    className="btn btn-outline-primary"
+                    onClick={() => addToCart(pro)}
+                    
+                    
+                    
 
-  products.filter(p => p.id === id)
-    
-  );
-} 
+                     >
+                       <i className="fa fa-plus" ></i>
+                        View
+                       </Link>
 
-const getProductByLink = (link) => {
-  let array = products.filter(product => product.link === link)
-  return array.length > 0 ? array[0] : null;
+          </div>
+      </div>
+    </div>
+  </div>
+
+   )
+
 }
-
-export default {getVisibleProducts,getVisibleProductsbyId, getProductByLink} ;
